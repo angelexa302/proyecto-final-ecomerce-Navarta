30e76718c9d2a5b22f3381c9bfbac36a82c76074
Jose Luis Navarta <joseluisnavarta@hotmail.com.ar>
2024-04-01 22:39:02 -0300

    Subiendo todo al servidor

---
+++ carrito.js
@@ -0,0 +1,95 @@
+document.addEventListener("DOMContentLoaded", mostrarProductosCarrito);
+
+function mostrarProductosCarrito() {
+  const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+  const listaProductosCarrito = document.getElementById("productos-carrito");
+
+ 
+  listaProductosCarrito.innerHTML = "";
+
+  if (carrito.length > 0) {
+    carrito.forEach((producto) => {
+      const li = document.createElement("li");
+      li.innerHTML = `${producto.titulo} - $${producto.precio}`;
+      listaProductosCarrito.appendChild(li);
+    });
+  } else {
+    listaProductosCarrito.innerHTML =
+      "<li>No hay productos en el carrito.</li>";
+  }
+}
+mostrarProductosCarrito();
+
+function actualizarCarrito() {
+  const productosCarrito = JSON.parse(localStorage.getItem("carrito")) || [];
+  const contenedorProductosCarrito =
+    document.getElementById("productos-carrito");
+  const totalCarritoProductos = document.getElementById("total-carrito");
+
+  contenedorProductosCarrito.innerHTML = "";
+
+  let totalCarrito = 0;
+
+  productosCarrito.forEach((producto) => {
+    const div = document.createElement("div");
+    div.innerHTML = `<img class="imagen-carrito" src="/${producto.imagen}"></img> <p>${producto.titulo} - $${producto.precio}</p>
+        <button onclick= "removerDelCarrito('${producto.id}')">Remover</button> 
+        `;
+    contenedorProductosCarrito.appendChild(div);
+    totalCarrito += producto.precio;
+  });
+  totalCarritoProductos.innerText = totalCarrito.toFixed(2);
+  
+}
+document.addEventListener("DOMContentLoaded", actualizarCarrito);
+
+document.getElementById("vaciar-carrito").addEventListener("click", () => {
+  localStorage.setItem("carrito", JSON.stringify([]));
+  actualizarCarrito();
+});
+
+function removerDelCarrito(productId) {
+  let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+  const nuevoCarrito = carrito.filter((producto) => producto.id !== productId);
+  localStorage.setItem("carrito", JSON.stringify(nuevoCarrito));
+  actualizarCarrito();
+}
+function actualizarTotalCarrito() {
+  let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+  let totalPrecio = carrito.reduce(
+    (total, producto) => total + producto.precio,
+    0
+  );
+  let totalArticulos = carrito.length;
+
+  document.getElementById(
+    "total-carrito"
+  ).textContent = `${totalPrecio.toFixed(2)}`;
+  document
+    .querySelector(".cart-summ")
+    .setAttribute("data-items-count", totalArticulos);
+  document.querySelector(
+    ".contador-carrito"
+  ).innerHTML = `Carrito ${totalArticulos} artículos de <span class="price-summ cart-totals"><span class="woocommerce-Price-amount amount"><bdi><span id="total-carrito">$${totalPrecio.toFixed(
+    2
+  )}</span></bdi></span></span>`;
+  const mensajeCarritoVacio = document.querySelector(".mensaje-carrito-vacio");
+  if(carrito.length > 0) {
+
+    mensajeCarritoVacio.style.display = "none";
+  } else {
+    mensajeCarritoVacio.style.display = "block";
+  }
+}
+
+function agregarAlCarrito(productoSeleccionado) {
+  let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+  carrito.push(productoSeleccionado);
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+  Swal.fire(`${productoSeleccionado.titulo} agregado al carrito!`);
+  actualizarTotalCarrito();
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  actualizarTotalCarrito();
+});
---
+++ main.js
@@ -0,0 +1,93 @@
+const URL = "/productos.json";
+let productos = [];
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+
+function cargarProductos(productos) {
+  contenedorProductos.innerHTML = "";
+  productos.forEach((producto) => {
+    const div = document.createElement("div");
+    div.classList.add("producto");
+    div.innerHTML = `<img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+    <div class="producto-detalles">
+      <h3 class="producto-titulo">${producto.titulo}</h3>
+      <p class="producto-precio">$${producto.precio}</p>
+      <button class="producto-agregar" id="${producto.id}">Agregar</button>
+    </div>`;
+    contenedorProductos.appendChild(div);
+    document
+      .getElementById(producto.id)
+      .addEventListener("click", () => agregarAlCarrito(producto));
+  });
+}
+
+function obtenerProductos() {
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      productos = data;
+      cargarProductos(productos);
+    })
+    .catch((error) => console.error("Error al cargar los productos:", error));
+}
+
+function mostrarProductos(categoria) {
+  const productosFiltrados = productos.filter(
+    (producto) => producto.categoria === categoria
+  );
+  cargarProductos(productosFiltrados);
+}
+
+document.getElementById("vinos-blancos").addEventListener("click", (e) => {
+  e.preventDefault();
+  mostrarProductos("Vinos Blancos");
+});
+document.getElementById("vinos-tintos").addEventListener("click", (e) => {
+  e.preventDefault();
+  mostrarProductos("Vinos Tintos");
+});
+document.getElementById("vinos-rosados").addEventListener("click", (e) => {
+  e.preventDefault();
+  mostrarProductos("Vinos Rosados");
+});
+
+obtenerProductos();
+
+function agregarAlCarrito(productoSeleccionado) {
+  let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+  carrito.push(productoSeleccionado);
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+  Swal.fire(`${productoSeleccionado.titulo} Agregado al carrito!`);
+}
+function actualizarTotalCarrito() {
+  let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+  let totalPrecio = carrito.reduce(
+    (total, producto) => total + producto.precio,
+    0
+  );
+  let totalArticulos = carrito.length;
+
+  document.getElementById(
+    "total-carrito"
+  ).textContent = `${totalPrecio.toFixed(2)}`;
+  document.querySelector(".cart-summ").setAttribute("data-items-count", totalArticulos);
+  document.querySelector(".contador-carrito").innerHTML = `Carrito ${totalArticulos} artículos de <span class="price-summ cart-totals"><span class="woocommerce-Price-amount amount"><bdi><span id="total-carrito">$${totalPrecio.toFixed(2)}</span></bdi></span></span>`;
+  const mensajeCarritoVacio = document.querySelector(".mensaje-carrito-vacio");
+  if(carrito.length > 0) {
+    mensajeCarritoVacio.style.display = "none";
+  } else {
+    mensajeCarritoVacio.style.display = "block";
+  }
+}
+
+function agregarAlCarrito(productoSeleccionado) {
+  let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+  carrito.push(productoSeleccionado);
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+  Swal.fire(`${productoSeleccionado.titulo} agregado al carrito!`);
+  actualizarTotalCarrito();
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  actualizarTotalCarrito();
+});
